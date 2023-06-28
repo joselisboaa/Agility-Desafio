d5110adf952684b728c3307957f9b23da05576a3
1mmanuelKant <joselisboaa05@gmail.com>
2023-06-28 19:54:10 -0300

    feat: add static login page

---
+++ src/app/(Login)/page.tsx
@@ -0,0 +1,9 @@
+import { LoginPage } from "@/features/Login/components/page"
+
+export default function Login () {
+  return (
+    <main className="flex min-h-screen flex-col items-center">
+      <LoginPage />
+    </main>
+  )
+}
