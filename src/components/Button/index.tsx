be5a0acc2c00042186d026c7eec1bd3ed450eebe
1mmanuelKant <joselisboaa05@gmail.com>
2023-06-28 17:32:40 -0300

    feat: button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,31 @@
+import { ButtonHTMLAttributes } from "react";
+import { twMerge } from "tailwind-merge";
+import classNames from "classnames";
+
+interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  id: string;
+  styles?: string;
+  color?: string;
+  variant?: string;
+}
+
+interface IButtonColors {
+  primary: string;
+}
+
+export const Button: React.FC<IButtonProps> = ({ color, children, styles, ...props }) => {
+
+  const colorVariants: IButtonColors = {
+    primary: "bg-primary hover:bg-primary-light",
+  };
+
+  const standardSize = "rounded-[5px] lg:w-[24.25rem] lg:h-[3.5rem]"
+
+  return (
+    <button className={twMerge(classNames(colorVariants["primary"], "rounded-[5px]", standardSize), styles)}>
+      <span className="text-white left-1/2 top-1/2 z-10 -translate-x-1/2 -translate-y-1/2">
+        {children}
+      </span>
+    </button>
+  );
+}
