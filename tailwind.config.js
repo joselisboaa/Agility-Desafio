31e73b38b5d550c59bf469691bce5b29eb93deb2
1mmanuelKant <joselisboaa05@gmail.com>
2023-06-28 17:09:13 -0300

    feat: add project tailwind configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,25 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 module.exports = {
+  important: true,
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/features/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+      colors: {
+        primary: '#2382A0',
+        "primary-light": "#2CB0D8",
+        'primary-background': '#FFFFFF',
+        "primary-dark": "#333333",
+        'secondary-dark': '#1A1A1B',
+        secondary: '#0F0F0F',
+        error: "#FF6961",
+        'primary-error': '#F21616',
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
