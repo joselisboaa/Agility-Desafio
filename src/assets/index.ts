0c90185c6b2211cc9aea22c7e9d60583269cbefd
1mmanuelKant <joselisboaa05@gmail.com>
2023-06-28 18:17:52 -0300

    feat: export eye img

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,1 +1,3 @@
 export { default as errorPageIcon } from "@/assets/img/error-page.svg";
+export { default as manWalkingByTrucks } from "@/assets/img/full-shot-man-walking-by-trucks-fleet 1.png";
+export { default as eyeShow } from "@/assets/img/eye.svg";
