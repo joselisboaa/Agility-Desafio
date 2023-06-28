4d1edc82139ef30af9fe1646807261d5244744b9
1mmanuelKant <joselisboaa05@gmail.com>
2023-06-28 17:32:29 -0300

    feat: add default colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,13 +11,16 @@ module.exports = {
     extend: {
       colors: {
         primary: '#2382A0',
-        "primary-light": "#2CB0D8",
+        "primary-light": "#7BB4C6",
         'primary-background': '#FFFFFF',
         "primary-dark": "#333333",
-        'secondary-dark': '#1A1A1B',
+        'secondary-dark': '#4F4F4F',
+        "tertiary-dark": "#828282",
+        "brand-green": "#00A843",
+        "brand-yellow": "#F3D11B",
+        "brand-green": "",
         secondary: '#0F0F0F',
-        error: "#FF6961",
-        'primary-error': '#F21616',
+        error: "#E54B4B",
       }
     }
   },
