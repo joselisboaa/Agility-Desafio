0aa547fc0190df42415e93cb0204b28be76d5e1e
joselisboaa <joselisboaa05@gmail.com>
2023-06-29 14:04:08 -0300

    feat: components export

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export * from "@/components/Button";
+export * from "@/components/Navbar";
+export * from "@/components/Header";
+export * from "@/components/QueryProvider";
+export * from "@/components/TextField";
+export * from "@/components/Logo";
