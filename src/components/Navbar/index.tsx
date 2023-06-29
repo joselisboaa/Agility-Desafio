10ef924b5fabbb158a422c536f2d2c4e994f0073
joselisboaa <joselisboaa05@gmail.com>
2023-06-29 16:02:50 -0300

    feat: change scroll bar width

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -5,7 +5,10 @@ export const Navbar = () => {
   return (
     <nav className="flex h-full max-h-[700px] w-[17.5rem] flex-col pl-4 pr-8">
       <h3 className="mb-4 pl-4 text-xs uppercase text-secondary-dark">Principal</h3>
-      <div className="overflow-y-auto">
+      <div
+        className="overflow-y-auto scrollbar scrollbar-track-slate-300 scrollbar-thumb-primary
+       scrollbar-thumb-rounded"
+      >
         <div className="flex min-h-[3.75rem] cursor-pointer items-center gap-4 pl-4 hover:bg-gray-color-40">
           <Image src={blueHomeIcon} alt="ir para a página inicial" className="fill-primary" />
           <span className="text-sm capitalize">página inicial</span>
