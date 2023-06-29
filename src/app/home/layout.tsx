c3f8dbc605fe8a9d30b509befc78876ac4b80814
joselisboaa <joselisboaa05@gmail.com>
2023-06-29 14:02:05 -0300

    feat: home page

---
+++ src/app/home/layout.tsx
@@ -0,0 +1,17 @@
+import { Header, Navbar } from "@/components";
+
+const teste = process.env.NEXT_PUBLIC_ACL_GOOGLE_API_KEY;
+
+interface IHomeLayout {
+  children: React.ReactNode;
+}
+
+export default function HomeLayout({ children }: IHomeLayout) {
+  return (
+    <div>
+      <Header username="José" />
+      <Navbar />
+      <div>{children}</div>
+    </div>
+  );
+}
