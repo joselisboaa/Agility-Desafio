c5c9f17818b3cb30ca539a5c0d8a57e367d45f4a
1mmanuelKant <joselisboaa05@gmail.com>
2023-06-28 23:58:47 -0300

    styles: change tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,25 +2,24 @@
 module.exports = {
   important: true,
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/features/**/*.{js,ts,jsx,tsx,mdx}'
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/features/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
       colors: {
-        primary: '#2382A0',
+        primary: "#2382A0",
         "primary-light": "#7BB4C6",
-        'primary-background': '#FFFFFF',
+        "primary-background": "#FFFFFF",
         "primary-dark": "#333333",
-        'secondary-dark': '#4F4F4F',
+        "secondary-dark": "#4F4F4F",
         "tertiary-dark": "#828282",
         "input-border": "##BDBDBD",
         "brand-green": "#00A843",
         "brand-yellow": "#F3D11B",
-        "brand-green": "",
-        secondary: '#0F0F0F',
+        secondary: "#0F0F0F",
         error: "#E54B4B",
       }
     }
