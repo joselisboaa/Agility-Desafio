729b094e726eded3f9b939caf93ef62878fe2dcb
joselisboaa <joselisboaa05@gmail.com>
2023-06-29 14:01:38 -0300

    feat: new assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -4,8 +4,12 @@ export { default as eyeShow } from "@/assets/img/eye.svg";
 export { default as menuRight } from "@/assets/img/Menu-right.svg";
 export { default as notification } from "@/assets/img/notification.svg";
 export { default as arrowDown } from "@/assets/img/arrow-down.svg";
+export { default as homeIcon } from "@/assets/img/home.svg";
+export { default as blueHomeIcon } from "@/assets/img/homeIcon.svg";
+export { default as blueMapIcon } from "@/assets/img/blueMapIcon.svg";
 
 // PNG assets
 export { default as manWalkingByTrucks } from "@/assets/img/full-shot-man-walking-by-trucks-fleet 1.png";
 export { default as aerialFactoryView } from "@/assets/img/aerial-view-factory-trucks-parked-near-warehouse-daytime 1.png";
 export { default as truckVehicle } from "@/assets/img/truck-vehicle-with-trailers-background 1.png";
+export { default as mapIcon } from "@/assets/img/map.png";
