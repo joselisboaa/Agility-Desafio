03a95fe066517e4908b0ef93404d501f23b0c526
1mmanuelKant <joselisboaa05@gmail.com>
2023-06-30 12:44:00 -0300

    feat: add responsive chart component

---
+++ src/components/Chart/index.tsx
@@ -0,0 +1,16 @@
+"use client";
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from "recharts";
+
+export const Chart = ({ data }) => {
+  return (
+    <ResponsiveContainer width="100%" height={500}>
+      <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="day" />
+        <YAxis />
+        <Tooltip />
+        <Line type="linear" dataKey="customer_quantity" stroke="#2382A0" />
+      </LineChart>
+    </ResponsiveContainer>
+  );
+};
