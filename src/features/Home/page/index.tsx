c52af3080de52ae6a5ea89ceb2fe38a0518df6b1
1mmanuelKant <joselisboaa05@gmail.com>
2023-06-30 12:48:22 -0300

    feat: make better response

--- src/features/Home/page/index.tsx
+++ src/features/Home/page/index.tsx
@@ -1,18 +1,24 @@
 import { Card } from "@/components/Card";
-import { arrowDown, profile, blueMapIcon, buildingIcon, cpuIcon } from "@/assets";
+import { arrowDown, profile, blueMapIcon, buildingIcon, cpuIcon, arrowLeft } from "@/assets";
 import { useFetch } from "@/hooks/useFetch";
 import { Ranking } from "../components/Ranking";
+import { Chart } from "@/components/Chart";
+import { ICustomersBody } from "@/provider";
+import classNames from "classnames";
+import Image from "next/image";
 
 export const HomeComponent = async () => {
   const sendCityHttpRequest = useFetch<[]>({ method: "GET", endpoint: "cities" });
   const sendClientHttpRequest = useFetch<[]>({ method: "GET", endpoint: "clients" });
   const sendSignatureHttpRequest = useFetch<[]>({ method: "GET", endpoint: "signatures" });
   const sendStoreHttpRequest = useFetch<[]>({ method: "GET", endpoint: "stores" });
+  const sendCustomersStatus = useFetch<ICustomersBody[]>({ method: "GET", endpoint: "customer-entry" });
 
   const clientResponse = await sendClientHttpRequest();
   const cityResponse = await sendCityHttpRequest();
   const signatureResponse = await sendSignatureHttpRequest();
   const storeResponse = await sendStoreHttpRequest();
+  const customerResponse = await sendCustomersStatus();
 
   const clientStatuses = clientResponse.length.toString();
   const cityStatuses = cityResponse.length.toString();
@@ -20,7 +26,7 @@ export const HomeComponent = async () => {
   const storeStatuses = storeResponse.length.toString();
 
   return (
-    <div className="flex h-full items-center justify-center">
+    <div className="flex h-full flex-col items-center justify-center gap-6 bg-primary-background">
       <section className="flex h-full w-full flex-col flex-wrap items-center gap-2 lg:ml-4 lg:mt-4 lg:flex-row lg:gap-4 xl:ml-6 xl:mt-6 2xl:ml-10 2xl:mt-10 2xl:gap-6">
         <Card countStatus={clientStatuses} icon={profile} status="Crescimento de " statusIcon={arrowDown} statusNumber="100%" title="Total de Clientes" />
         <Card countStatus={cityStatuses} icon={blueMapIcon} status="Crescimento de " statusIcon={arrowDown} statusNumber="100%" title="Total de Cidades" />
@@ -34,7 +40,16 @@ export const HomeComponent = async () => {
           title="Total de Lojas parceiras"
         />
       </section>
-      <Ranking />
+      <section className="flex w-full flex-wrap justify-center gap-6 p-4">
+        <div className="hidden w-[calc(100%-350px)] flex-col gap-6 lg:flex">
+          <div className="flex gap-2">
+            <Image src={arrowLeft} alt="ícone sinalizando a entrada de clientes" />
+            <h2 className="text-2xl font-semibold text-secondary-dark">Entrada de clientes - {customerResponse[0].month}</h2>
+          </div>
+          <Chart data={customerResponse} />
+        </div>
+        <Ranking />
+      </section>
     </div>
   );
 };
