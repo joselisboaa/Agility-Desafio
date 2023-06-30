498dfd2cafea1dbff9ee3230b0af53b877c83123
1mmanuelKant <joselisboaa05@gmail.com>
2023-06-29 23:32:42 -0300

    feat: get user data and set background color

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,4 +1,5 @@
 import { Header, Navbar } from "@/components";
+import { useFetch } from "@/hooks/useFetch";
 
 const GOOGLE_API_KEY = process.env.NEXT_PUBLIC_ACL_GOOGLE_API_KEY;
 
@@ -6,12 +7,24 @@ interface IHomeLayout {
   children: React.ReactNode;
 }
 
-export default function HomeLayout({ children }: IHomeLayout) {
+interface IUserAccountProps {
+  id: number;
+  login: string;
+  password: string;
+}
+
+export default async function HomeLayout({ children }: IHomeLayout) {
+  const sendHttpRequest = useFetch<IUserAccountProps[]>({ method: "GET", endpoint: "accounts" });
+
+  const [userData] = await sendHttpRequest();
+
   return (
     <div>
-      <Header username="José" />
-      <Navbar />
-      <div>{children}</div>
+      <Header username={userData.login} />
+      <div className="flex">
+        <Navbar />
+        <div className="bg-primary-brand-dark w-full">{children}</div>
+      </div>
     </div>
   );
 }
